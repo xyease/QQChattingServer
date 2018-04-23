97b2da4a14ac484faca27a84916ba6f764832393
ease <1547770648@qq.com>
2018-04-23 11:19:16 +0800

    Reorganization

---
+++ src/CommonClass/CommandIndex.java
@@ -0,0 +1,5 @@
+package CommonClass;
+
+public enum CommandIndex {
+    Register,Login
+}
--- src/CommonClass/Usr.java
+++ src/CommonClass/Usr.java
@@ -1,16 +1,18 @@
 package CommonClass;
 import java.io.Serializable;
 public class Usr implements Serializable{
+	 private CommandIndex CommandNum;
 	 private String usrname;
 	 private String password;
-    public Usr(String name,String pass) {
-   	  usrname=name;
-   	  password=pass;
-    }
-    public String GetName() {
-   	 return usrname;
-    }
-    public String GetPass() {
-   	 return password;
-    }
+     public Usr(CommandIndex num,String name,String pass) {
+    	  CommandNum=num;
+    	  usrname=name;
+    	  password=pass;
+     }
+     public String GetName() {
+    	 return usrname;
+     }
+     public String GetPass() {
+    	 return password;
+     }
 }
